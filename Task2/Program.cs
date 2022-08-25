237ba635f5998ab9f3d47745f94ab13526488172
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 23:08:57 +0300

    Решена задача на вывод третьй цифры числа

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int Anumber = int.Parse(Console.ReadLine());
+string Atxt = Convert.ToString(Anumber);
+if (Anumber>2)
+{
+    Console.WriteLine($"третья цифра " + Atxt[2]);
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
